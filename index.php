aa5bb4bc322ea934038791cfa559617ab4802508
LUISF403 <luis.hyrb.15@hotmail.com>
2015-10-28 09:09:44 -0600

    iSchool
    Pagina web de la profe cynthia

---
+++ index.php
@@ -0,0 +1,210 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Bootstrap, a sleek, intuitive, and powerful mobile first front-end framework for faster and easier web development.">
+<meta name="keywords" content="HTML, CSS, JS, JavaScript, framework, bootstrap, front-end, frontend, web development">
+<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+<meta name="google-site-verification" content="h9NYnax1vhzr5lO7ZBWCJyA-oyqTN69HrF1bO2K5cJs" />
+
+<title>
+    iSchool
+</title>
+
+
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/docs.min.css" rel="stylesheet">
+
+<!--[if lt IE 9]><script src="../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+<script src="js/ie-emulation-modes-warning.js"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!-- Favicons -->
+<link rel="apple-touch-icon" href=" images/apple-touch-icon.png">
+<link rel="icon" href="images/favicon.ico">
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-146052-10', 'getbootstrap.com');
+  ga('send', 'pageview');
+</script>
+
+  </head>
+  <body class="bs-docs-home">
+    <!-- Docs master nav -->
+  <header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
+  <div class="" style="background-color:#e67e22">
+    
+    <nav id="bs-navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Expo</a>
+        </li>
+        <li>
+          <a href="">Blog</a>
+        </li>
+        <li>
+          <a href="">stuf</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+
+    <!-- Page content of course! -->
+    <main class="bs-docs-masthead" id="content" role="main" tabindex="-1">
+  <div class="container">
+    <span class="bs-docs-booticon bs-docs-booticon-lg"><img src="images/nlogo.png" alt="" class="img-responsive" ></span>
+    <p class="lead">iSchool</p>
+    <p class="lead">
+      <a href="getting-started#download" class="btn btn-outline-inverse btn-lg" onclick="ga('send', 'event', 'Jumbotron actions', 'Download', 'Download 3.3.5');">Download </a>
+    </p>
+    <p class="version">Currently v0.1</p>
+    <div id="carbonads-container"><div class="carbonad"><div id="azcarbon"></div><script>var z = document.createElement("script"); z.async = true; z.src = "http://engine.carbonads.com/z/32341/azcarbon_2_1_0_HORIZ"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(z, s);</script></div></div>
+
+  </div>
+</main>
+
+<div class="bs-docs-featurette">
+  <div class="container">
+    <h2 class="bs-docs-featurette-title">Designed for everyone</h2>
+    <p class="lead"></p>
+
+    <hr class="half-rule">
+
+    <div class="row">
+      <div class="col-sm-4">
+        <img src="images/estudiante.jpg" alt="Sass and Less support" class="img-responsive center-block" width="200px">
+        <h3 class="text-center">Students</h3>
+        <span>
+          For those students who need help with school procedures,
+           to locate school buildings or anything related to school
+        </span>
+      </div>
+      <div class="col-sm-4 ">
+        <img src="images/maestro.png" alt="Responsive across devices" class="img-responsive center-block" width="220px">
+        <h3>Professor</h3>
+        <span>
+            For those teachers who want to be aware of school news,
+             events on the institution, guide their students.
+        </span> 
+      </div>
+      <div class="col-sm-4">
+        <img src="images/usuario.png" alt="Components" class="img-responsive center-block" width="200px">
+        <h3>Users</h3>
+        <p>And finally for those outside the institution wishing to obtain information 
+          about the institution,its educational offerings, the school diary,
+           school events and etc.</p>
+      </div>
+    </div>
+
+    <hr class="half-rule">
+
+    <p class="lead">iSchool intended as a tool to make school life easier</p>
+  </div>
+</div>
+
+<div class="bs-docs-featurette">
+  <div class="container">
+    <h2 class="bs-docs-featurette-title">Building the future</h2>
+    <p class="lead">Join the technology revolution and is the first at the Institute Tecnologico to download iSchool and facilitate as a student, teacher or institution wing outsider.</p>
+
+    <hr class="half-rule">
+
+    <div class="row bs-docs-featured-sites">
+        <div class="col-xs-3 col-sm-3">
+          <img src="images/ipro1.png" alt="Vogue" class="img-responsive center-block" width="150px">
+        </div>
+        <div class="col-xs-3 col-sm-3">
+          <img src="images/ipro2.png" alt="Riot Design" class="img-responsive center-block" width="150px">
+        </div>
+        <div class="col-xs-3 col-sm-3">
+          <img src="images/ipro3.png" alt="Vogue" class="img-responsive center-block" width="150px">
+        </div>
+        <div class="col-xs-3 col-sm-3">
+          <img src="images/ipro4.png" alt="Vogue" class="img-responsive center-block" width="150px">
+        </div>
+    </div>
+  </div>
+</div>
+
+
+
+    <!-- Footer
+================================================== -->
+<footer class="bs-docs-footer" role="contentinfo">
+  <div class="container">
+    <div class="bs-docs-social">
+  <ul class="bs-docs-social-buttons">
+    <li class="follow-btn">
+      <a href="" class="twitter-follow-button" data-link-color="#0069D6" data-show-count="true">Follow @iSchool</a>
+    </li>
+    <li class="tweet-btn">
+      <a href="" class="twitter-share-button" data-url="http://getbootstrap.com/" data-count="horizontal" data-via="getbootstrap" >Tweet</a>
+    </li>
+  </ul>
+
+</footer>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+
+  <script src="js/bootstrap.min.js"></script>
+
+
+
+  <script src="js/docs.min.js"></script>
+
+
+
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="js/ie10-viewport-bug-workaround.js"></script>
+
+
+<script>
+  window.twttr = (function (d,s,id) {
+    var t, js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return; js=d.createElement(s); js.id=id; js.async=1;
+    js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+    return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+  }(document, "script", "twitter-wjs"));
+</script>
+
+<!-- Analytics
+================================================== -->
+<script>
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '4f0dc9fef5a1f55508000013');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
+</script>
+
+  </body>
+</html>
